5c9e816c148d6de39fc2824f9ed837ece71a5cc9
unknown <grib.vladimir1990@gmail.com>
2020-03-18 01:45:04 +0200

    last-ornot

--- js/main.js
+++ js/main.js
@@ -2,13 +2,6 @@ document.querySelector(".right ul li").addEventListener("click", function(){
     this.classList.toggle("active");
 });
 
-document.querySelector(".left-dropdown ul li").addEventListener("click", function(){
-    this.classList.toggle("active");
-});
-document.querySelector(".left-dropdown-mobil ul li").addEventListener("click", function(){
-    this.classList.toggle("active");
-});
-
 let menuToggle = document.querySelector('#menu-togle');
 let mobileNavContainer = document.querySelector('#mobile-nav');
 
--- js/main1.js
+++ js/main1.js
@@ -1,20 +1,17 @@
-const menuToggle = document.querySelector('#menu-togle');
-const mobileNavContainer = document.querySelector('#mobile-nav');
+document.querySelector(".right ul li").addEventListener("click", function(){
+    this.classList.toggle("active");
+});
+document.querySelector(".left-dropdown ul li").addEventListener("click", function(){
+    this.classList.toggle("active");
+});
+document.querySelector(".left-dropdown-mobil ul li").addEventListener("click", function(){
+    this.classList.toggle("active");
+});
+
+let menuToggle = document.querySelector('#menu-togle');
+let mobileNavContainer = document.querySelector('#mobile-nav');
 
 menuToggle.onclick = function(){
     menuToggle.classList.toggle('menu-icon-active');
     mobileNavContainer.classList.toggle('mobile-nav--active');
 };
-
-jQuery(function () {
-    jQuery(document).on('mousemove', function (e) {
-        jQuery('#layout1').css({
-            left: -e.pageX / 100 + 20,
-            top: -e.pageY / 100 + 0
-        }); 
-        jQuery('#layout2').css({
-            right: -e.pageX / 100 - 0,
-            top: -e.pageY / 100 + 0
-        });       
-    });
-});
