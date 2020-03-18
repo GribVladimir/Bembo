7ad8d6c253d13383291f3959b4ad718b50f8af27
unknown <grib.vladimir1990@gmail.com>
2020-03-18 13:35:39 +0200

    last-ornot2

---
+++ js/main3.js
@@ -0,0 +1,20 @@
+
+let menuToggle = document.querySelector('#menu-togle');
+let mobileNavContainer = document.querySelector('#mobile-nav');
+
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-nav--active');
+};
+jQuery(function () {
+    jQuery(document).on('mousemove', function (e) {
+        jQuery('#layout1').css({
+            left: -e.pageX / 100 + 20,
+            top: -e.pageY / 100 + 0
+        }); 
+        jQuery('#layout2').css({
+            right: -e.pageX / 100 - 0,
+            top: -e.pageY / 100 + 0
+        });       
+    });
+});
